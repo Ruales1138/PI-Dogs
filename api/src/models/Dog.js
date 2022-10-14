d7649ce573224cd4606c231aca8d22c0ae689e50
unknown <hruales1138@gmail.com>
2022-10-14 14:32:55 -0500

    Pi

--- api/src/models/Dog.js
+++ api/src/models/Dog.js
@@ -4,9 +4,46 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('dog', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+
     name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
-  });
+
+    bred_for: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+
+    breed_group: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+
+    life_span: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+
+    temperament: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+
+    origin: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+
+    image: {
+      type: DataTypes.STRING,
+      allowNull: true
+    }
+  },
+  {timestamps: false});
 };

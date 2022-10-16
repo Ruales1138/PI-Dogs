01d05cc980ecf16f66c1e736a27ded108057fdc4
unknown <hruales1138@gmail.com>
2022-10-15 21:05:47 -0500

    PI

--- api/src/routes/dogsRouter.js
+++ api/src/routes/dogsRouter.js
@@ -1,15 +1,38 @@
 const { Router } = require('express');
-const { dogsData } = require('../controllers/dogsController')
+const { Dog } = require('../db');
 
 const router = Router();
 
 router.get('/', async (req, res) => {
     const { name } = req.query;
     try {
+        if(!name) {
+            const allDogs = await Dog.findAll();
+            res.send(allDogs);
+        } 
+        else {
+            const aux = {};
+            aux.name = name;
+            const filterDog = await Dog.findAll({ where: aux });
+            res.send(filterDog)
+        }
         
     } catch (error) {
-        res.status(404).send('error')
+        res.status(404).send(error.message);
     }
 });
+
+router.get('/:id', async (req, res) => {
+    const { id } = req.params;
+    try {
+        const aux = {};
+        aux.id = id;
+        const filterDog = await Dog.findAll({ where: aux });
+        res.send(filterDog)
+        
+    } catch (error) {
+        res.status(404).send(error.message);
+    }
+})
 
 module.exports = router;
